40b7db58f3d504a64ea0fa131492910d94aa3ca4
Alan21103 <syahlansaputra03@gmail.com>
2025-05-21 20:06:31 +0700

    Menambahkan icon search dan mengatur posisi dan jarak di textfromfield

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:plant_app/constraints.dart';
 
 class Body extends StatelessWidget {
@@ -51,7 +52,30 @@ class Body extends StatelessWidget {
                             hintStyle: TextStyle(
                               color: kPrimaryColor.withOpacity(0.5),
                             ),
-                            contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 16,
+                              horizontal: 16,
+                            ),
+
+                            // Icon di kanan (rapi sejajar dengan teks)
+                            suffixIcon: Container(
+                              margin: EdgeInsets.only(
+                                right: 12,
+                              ), // Geser sedikit dari kanan
+                              child: SvgPicture.asset(
+                                "assets/icons/search.svg",
+                                colorFilter: ColorFilter.mode(
+                                  Colors.grey,
+                                  BlendMode.srcIn,
+                                ),
+                                width: 24,
+                                height: 24,
+                              ),
+                            ),
+                            suffixIconConstraints: BoxConstraints(
+                              minHeight: 24,
+                              minWidth: 24,
+                            ),
                             enabledBorder: InputBorder.none,
                             focusedBorder: InputBorder.none,
                           ),
