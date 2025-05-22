9038b4eb24759cebaa16e9f55d4cae95d6a34fec
Alan21103 <syahlansaputra03@gmail.com>
2025-05-22 19:31:00 +0700

    Menambahkan title pada profile page

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:plant_app/components/my_bottom_nav_bar.dart';
+import 'package:plant_app/screens/home/home_screen.dart';
 import 'package:plant_app/screens/profile/components/body_profile.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -11,19 +12,32 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(context),
       body: BodyProfile(),
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: const MyBottomNavBar(currentIndex: 2),
     );
   }
 
-  AppBar buildAppBar(BuildContext context){
+  AppBar buildAppBar(BuildContext context) {
     return AppBar(
+      title: const Text(
+        'My Profile',
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+      centerTitle: true,
       elevation: 0,
       leading: IconButton(
-        icon: SvgPicture.asset("assets/icons/back_arrow (1).svg",
-         colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        icon: SvgPicture.asset(
+          "assets/icons/back_arrow (1).svg",
+          colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
         ),
-        onPressed: (){
-          Navigator.pop(context);
+        onPressed: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+          );
         },
       ),
     );
