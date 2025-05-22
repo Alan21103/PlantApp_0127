d4a52d8d8a4212896dd2d7cc328cc7bb16aadf67
Alan21103 <syahlansaputra03@gmail.com>
2025-05-22 14:24:39 +0700

    Membuat header di profile menggunakan warna yang sama dengan home page

---
+++ lib/screens/profile/components/header_profile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constraints.dart';
+
+class HeaderProfile extends StatelessWidget {
+  const HeaderProfile({super.key, required this.size});
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size.height * 0.25,
+      color: kPrimaryColor, // Light purple
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          padding: const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white, width: 4),
+          ),
+          child: const CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage("assets/images/logo1.jpg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
