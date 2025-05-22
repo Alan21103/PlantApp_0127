1fd4d9bcaabf16e61ea4770e019d5300e33d1bd0
Alan21103 <syahlansaputra03@gmail.com>
2025-05-22 14:08:24 +0700

    extract widget dan dipanggil di body home

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,118 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:plant_app/constraints.dart';
+import 'package:plant_app/screens/home/components/featured_plants.dart';
+import 'package:plant_app/screens/home/components/header_withSearchBox.dart';
+
+import 'recomend_plants.dart';
+import 'title_with_more_bbtn.dart';
 
 class Body extends StatelessWidget {
-  const Body({super.key});
-
   @override
   Widget build(BuildContext context) {
+    // It will provie us total height  and width of our screen
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        // ignore: sized_box_for_whitespace
-        Container(
-          height: size.height * 0.2,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                  left: kDefaultPadding,
-                  right: kDefaultPadding,
-                  bottom: 36 + kDefaultPadding,
-                ),
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      'Hi Alan!',
-                      style: Theme.of(
-                        context,
-                      ).textTheme.headlineMedium?.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Spacer(),
-                    ClipOval(
-                      child: Image.asset(
-                        "assets/images/logo1.jpg",
-                        width: 60, 
-                        height: 60,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
-                            ),
-                            // Icon di kanan (rapi sejajar dengan teks)
-                            suffixIcon: Container(
-                              margin: EdgeInsets.only(
-                                right: 12,
-                              ), // Geser sedikit dari kanan
-                              child: SvgPicture.asset(
-                                "assets/icons/search.svg",
-                                colorFilter: ColorFilter.mode(
-                                  Colors.grey,
-                                  BlendMode.srcIn,
-                                ),
-                                width: 24,
-                                height: 24,
-                              ),
-                            ),
-                            suffixIconConstraints: BoxConstraints(
-                              minHeight: 24,
-                              minWidth: 24,
-                            ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
     );
   }
 }
